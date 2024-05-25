7b29297b968825e7970b75aec99de67953aa59a8
nightmaregit <nightmaregit@gmail.com>
2024-05-25 23:19:17 +0700

    perbarui tugas

--- tugas/tugas_parkir.php
+++ tugas/tugas_parkir.php
@@ -1,8 +1,9 @@
 <?php
-    // buat tampilan sesuai dengan tugas
-     function hitungTarif($a, $b) { 
-        return $a*$b;
-    }
+// buat tampilan sesuai dengan tugas
+function hitungTarif($a, $b)
+{
+  return $a * $b;
+}
 ?>
 
 <html>
@@ -13,40 +14,40 @@
 <?php
 
 if (isset($_GET['jumlah'])) {
-   // mengambil data dari URL
-   // panggil function hitungTarif
-   $noPolisi = $_GET['noPolisi'];
-   $lamaParkir = $_GET['lamaParkir'];
-   $harga = $_GET['harga'];  
-   $hasil = hitungTarif($lamaParkir, $harga);
-   # code...
+  // mengambil data dari URL
+  // panggil function hitungTarif
+  $noPolisi = $_GET['noPolisi'];
+  $lamaParkir = $_GET['lamaParkir'];
+  $harga = $_GET['harga'];
+  $hasil = hitungTarif($lamaParkir, $harga);
+  # code...
 }
 
 ?>
 <form action="" method="get">
-<div class="wrapper">
-      
-      <div class="input-box">
-        <input type="text" name="noPolisi" placeholder=" No polisi" />
-      </div>
-      
-      <div class="input-box">
-        <input type="text" name="lamaParkir" placeholder=" Lama parkir" />
-      </div>
-      
-      <div class="input-box">
-        <input type="text" name="harga" placeholder=" Tarif / jam" />
-      </div>
-      
-      <div class="input-box">
-        <input type="submit" name="jumlah" value="Hitung" />
-      </div>
-      
-      <div class="input-box">
-        <input type="text" name="" value="<?php print(isset($hasil)) ? $hasil : "Total" ; ?>" readonly/>
-      </div>
-      
+  <div class="wrapper">
+
+    <div class="input-box">
+      <input type="text" name="noPolisi" placeholder=" No polisi" />
     </div>
+
+    <div class="input-box">
+      <input type="text" name="lamaParkir" placeholder=" Lama parkir" />
+    </div>
+
+    <div class="input-box">
+      <input type="text" name="harga" placeholder=" Tarif / jam" />
+    </div>
+
+    <div class="input-box">
+      <input type="submit" name="jumlah" value="Hitung" />
+    </div>
+
+    <div class="input-box">
+      <input type="text" name="" value="<?php print (isset($hasil)) ? $hasil : "Total"; ?>" readonly />
+    </div>
+
+  </div>
 </form>
 
 </html>
