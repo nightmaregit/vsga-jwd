fceb6f577bd7321624059e5b1dc07a7b388e1930
nightmaregit <nightmaregit@gmail.com>
2024-05-25 22:49:23 +0700

    perbarui tugas parkir

--- tugas/tugas_parkir.php
+++ tugas/tugas_parkir.php
@@ -7,30 +7,46 @@
 
 <html>
 
+<link rel="stylesheet" href="tugas_parkir.css">
+
+
+<?php
+
+if (isset($_GET['jumlah'])) {
+   // mengambil data dari URL
+   // panggil function hitungTarif
+   $noPolisi = $_GET['noPolisi'];
+   $lamaParkir = $_GET['lamaParkir'];
+   $harga = $_GET['harga'];  
+   $hasil = hitungTarif($lamaParkir, $harga);
+   # code...
+}
+
+?>
 <form action="" method="get">
-    No Polisi : <input type="text" name="noPolisi" > <br><br>
-    lama parkir : <input type="text" name="lamaParkir" > <br><br>
-    harga : <input type="text" name="harga" > <br><br>
-    jumlah : <input type="submit" name="jumlah" value="Tarif parkir " > <br><br>
-   
-</form> 
-
- <?php
-
- if (isset($_GET['jumlah'])) {
-    // mengambil data dari URL
-    // panggil function hitungTarif
-    $noPolisi = $_GET['noPolisi'];
-    $lamaParkir = $_GET['lamaParkir'];
-    $harga = $_GET['harga'];  
-    $hasil = hitungTarif($lamaParkir, $harga);
-    
-    echo "tarif parkir kendaraan dengan nomor polisi $noPolisi = $hasil";
-    # code...
- }
-
-  
-
- ?>
+<div class="wrapper">
+      
+      <div class="input-box">
+        <input type="text" name="noPolisi" placeholder=" No polisi" />
+      </div>
+      
+      <div class="input-box">
+        <input type="text" name="lamaParkir" placeholder=" Lama parkir" />
+      </div>
+      
+      <div class="input-box">
+        <input type="text" name="harga" placeholder=" Tarif / jam" />
+      </div>
+      
+      <div class="input-box">
+        <input type="submit" name="jumlah" value="Hitung" />
+      </div>
+      
+      <div class="input-box">
+        <input type="text" name="" value="<?php print(isset($hasil)) ? $hasil : "Total" ; ?>" readonly/>
+      </div>
+      
+    </div>
+</form>
 
 </html>
